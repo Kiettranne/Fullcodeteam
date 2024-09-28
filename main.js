576ed1e480edcb96b40b2518db87b926fcb4c243
Nguyễn Hoàng Huynh <nguyenhoanghuynh.vn@gmail.com>
2024-09-28 23:56:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 window.addEventListener('scroll', function() {
-    const header = document.querySelectorAll('#header');
-    const scrollPosition = window.scrollY + window.innerHeight / 2;
-
-    header.forEach(header => {
-        if (scrollPosition >= header.offsetTop && scrollPosition < header.offsetTop + header.offsetHeight) {
-            header.style.backgroundColor = 'white'; // Thay đổi màu nền khi cuộn đến phần này
-        } else {
-            header.style.backgroundColor = 'white';
-        }
-    });
+    var header = document.getElementById('header');
+    if (window.scrollY === 0) {
+        header.classList.remove('scrolled');
+    } else {
+        header.classList.add('scrolled');
+    }
 });
